e7fb20935a99fa60f032559c8a7395aeec890526
Nail <nail.gu.hunter@gmail.com>
2021-01-23 17:45:47 +0800

    :sparkles: (components) render-schema component support events handlers

--- src/components/schema-render/core/render.ts
+++ src/components/schema-render/core/render.ts
@@ -1,10 +1,13 @@
-import { DefineComponent, h, VNode } from 'vue'
+import { DefineComponent, h, Ref, ref, VNode } from 'vue'
+
+type Events = Record<string, string | string[]>;
 
 export interface Schema {
   name: DefineComponent | string;
 
   props?: Record<string, any>;
   attrs?: Record<string, string>;
+  events?: Events;
 
   slots?: {
     [name: string]: Schema;
@@ -35,9 +38,13 @@ export default function render (schema: Schema): VNode {
     }, {} as any);
   }
 
+  const idRef = ref(null)
+
   const others = {
+    ref: idRef,
     ...get(schema, 'props', {}),
     ...get(schema, 'attrs', {}),
+    ...parseEvents(get(schema, 'events', {}), idRef),
   }
 
   return h(schema.name, others, childrenOrSlots);
@@ -45,4 +52,40 @@ export default function render (schema: Schema): VNode {
 
 function get<T extends object>(object: T, key: keyof T, defaultValue: any) {
   return key in object ? object[key] : defaultValue;
+}
+
+function parseEvents(events: Events, ref: Ref<any>) {
+  const eventHandlers: Record<string, Function> = {};
+
+  for (let event in events) {
+    // @ts-ignore
+    const handlers: string[] = [].concat(events[event]);
+
+    // TODO think a bette way to handle it
+    // TODO exec order: make this to be a function chain ( what about promise chain? )
+    // TODO it can only call itself functions now, support call other refs' functions
+    // TODO support pass parameters by schema
+    eventHandlers[event] = () => {
+      const instance = ref.value
+
+      // TODO make funcs
+      handlers.map((handler: string) => {
+        // TODO better type infer
+        // @ts-ignore
+        const func = instance[handler];
+
+        if (typeof func === 'function') {
+          return func()
+        } else {
+          logger(`${handler} is not a funciton!`)
+        }
+      })
+    }
+  }
+
+  return eventHandlers
+}
+
+function logger(...msg: any) {
+  console.log(...msg)
 }
