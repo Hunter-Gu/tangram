20ccaa7c662135d46d56c67aecccb8409101ea2c
Nail <nail.gu.hunter@gmail.com>
2021-07-12 01:02:58 +0800

    feat(pages/editor/enhance-prop): support auto wrap whole item  when prop name wrapped

--- src/pages/editor/transformer/components/enhance-prop/enhance-prop.vue
+++ src/pages/editor/transformer/components/enhance-prop/enhance-prop.vue
@@ -1,13 +1,13 @@
 <template>
-  <el-space size="mini" spacer=":">
-    <span>{{ label || name }}</span>
+  <el-space :class="wrap && 'enable-wrap'" size="mini" spacer=":" :wrap="wrap">
+    <span ref="labelRef">{{ label || name }}</span>
     <slot></slot>
   </el-space>
 </template>
 
 <script lang="ts" setup>
-import { defineProps } from "@vue/runtime-core";
-import type { PropType } from "@vue/runtime-core";
+import { defineProps, onMounted, ref } from "@vue/runtime-core";
+import type { PropType, Ref } from "@vue/runtime-core";
 import type { ComponentProp } from "../../../types/component";
 
 const props: ComponentProp = defineProps({
@@ -18,4 +18,25 @@ const props: ComponentProp = defineProps({
     type: String as PropType<string>,
   },
 }); // TODO why props missing when add `as ComponentProp` at tail
+
+const useWrap = (ref: Ref) => {
+  const elm = ref.value;
+  const lineHeight = parseInt(getComputedStyle(elm).lineHeight);
+  const height = parseInt(elm.getBoundingClientRect().height);
+
+  return height > 1.5 * lineHeight;
+};
+
+const labelRef = ref();
+let wrap = ref(false);
+
+onMounted(() => {
+  wrap.value = useWrap(labelRef);
+});
 </script>
+
+<style scoped>
+.enable-wrap.el-space > :deep(.el-space__item):last-child {
+  width: 100%;
+}
+</style>
