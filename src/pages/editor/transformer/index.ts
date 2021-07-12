1d7209cb8d08961e6f899db0d5587901c4b5fda3
Nail <nail.gu.hunter@gmail.com>
2021-07-12 23:43:18 +0800

    feat(pages/editor/transformer): add default transformer config

--- src/pages/editor/transformer/index.ts
+++ src/pages/editor/transformer/index.ts
@@ -1,12 +1,13 @@
 import { DescriptorPropTypes } from "../types/descriptor";
 import { transformer } from "./transformer";
+import Wrapper from "./components/wrapper";
+import { ElInput, ElSwitch } from "element-plus";
 
 export * from "./transform";
 export * from "./transformer";
 
-// TODO default component
 transformer
-  .configRoot("div")
-  .configGlobal(DescriptorPropTypes.Boolean, "div")
-  .configGlobal(DescriptorPropTypes.String, "div")
-  .configGlobal(DescriptorPropTypes.Number, "div");
+  .configRoot(Wrapper)
+  .configGlobal(DescriptorPropTypes.Boolean, ElSwitch)
+  .configGlobal(DescriptorPropTypes.String, ElInput)
+  .configGlobal(DescriptorPropTypes.Number, ElInput);
