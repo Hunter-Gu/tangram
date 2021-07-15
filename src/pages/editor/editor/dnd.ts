a35512a22ea040090a766cf230ac47f8c47c8820
Nail <nail.gu.hunter@gmail.com>
2021-07-16 00:46:43 +0800

    feat(editor/dnd): update schema data of store when drop

---
+++ src/pages/editor/editor/dnd.ts
@@ -0,0 +1,26 @@
+import { Mutations, State } from "../../../plugins/store";
+import { Store, useStore } from "vuex";
+
+export function useDrag(dragElm: HTMLElement) {
+  dragElm.draggable = true;
+
+  dragElm.addEventListener("dragstart", (evt: DragEvent) => {
+    const name = (evt.target as HTMLElement).dataset.name!;
+
+    evt.dataTransfer?.setData("text", name);
+  });
+}
+
+export function useDrop(dropElm: HTMLElement) {
+  const store: Store<State> = useStore();
+
+  dropElm.addEventListener("dragover", (evt: Event) => {
+    evt.preventDefault();
+  });
+
+  dropElm.addEventListener("drop", (evt: DragEvent) => {
+    const name = evt.dataTransfer?.getData("text");
+
+    store.commit(Mutations.ADD, name);
+  });
+}
