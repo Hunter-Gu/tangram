c517279b9516cee27d652686a24a3a92f311bce0
Nail <nail.gu.hunter@gmail.com>
2021-08-05 00:01:14 +0800

    feat(plugins/store): support select current select component instance

--- src/plugins/store.ts
+++ src/plugins/store.ts
@@ -1,9 +1,10 @@
-import { SchemaData } from "../core/parser/src/types/schema";
+import { SchemaData, Component, Child } from "../core/parser/src/types/schema";
 import { createLogger } from "../utils/logger";
 import { createStore } from "vuex";
 import { registry } from "../pages/editor/registry";
 import { PropsDescriptor } from "@/pages/editor/types/descriptor";
 import { get } from "../core/parser/src/utils/utils";
+import Block from "../pages/editor/block";
 
 const logger = createLogger("store");
 
@@ -13,6 +14,8 @@ export type State = {
   currentSelect?: PropsDescriptor;
 
   currentPath: string;
+
+  selectIndexs: number[];
 };
 
 export enum Mutations {
@@ -20,10 +23,16 @@ export enum Mutations {
   ADD_ELEMENT = "add",
 
   // eslint-disable-next-line no-unused-vars
-  SELECT_ELEMENT = "select",
+  SET_CURRENT_SELECT = "set_current_select",
+
+  // eslint-disable-next-line no-unused-vars
+  SELECT = "select",
 
   // eslint-disable-next-line no-unused-vars
   UPDATE_ELEMENT_PROPS = "update",
+
+  // eslint-disable-next-line no-unused-vars
+  CLEAR_SELECTS = "clear_selects",
 }
 
 export const store = createStore<State>({
@@ -34,9 +43,14 @@ export const store = createStore<State>({
       children: [],
     },
 
+    // current selected component's props
     currentSelect: undefined,
 
     currentPath: "",
+
+    // the index of Block which has been selected
+    // may can be a number
+    selectIndexs: [],
   },
 
   mutations: {
@@ -45,23 +59,29 @@ export const store = createStore<State>({
         const renderDescriptor =
           registry.getPropsDescriptor(componentName)?.data;
 
-        const childrenCount = state.schema.children?.push({
-          name: renderDescriptor?.component,
-          __uuid: new Date().getTime(),
-        })!;
+        const component = renderDescriptor?.component;
+        const index = state.schema.children?.length ?? 0;
 
-        store.commit(Mutations.SELECT_ELEMENT, {
-          descriptor: renderDescriptor?.descriptor,
-          index: childrenCount - 1,
-        });
+        state.schema.children?.push(enhanceBlock(component, index))!;
+        store.commit(Mutations.SELECT, { name: component?.name, index });
       } catch (e) {
         logger.error(e.message);
       }
     },
 
-    [Mutations.SELECT_ELEMENT](state, { descriptor, index }) {
-      state.currentPath = `children.${index}`;
-      state.currentSelect = descriptor;
+    [Mutations.SELECT](state, { name, index }) {
+      const blockPath = `children.0`;
+      const renderDescriptor = registry.getPropsDescriptor(name)?.data;
+
+      state.currentPath = `.children.${index}.${blockPath}`;
+
+      state.currentSelect = renderDescriptor?.descriptor;
+
+      state.selectIndexs = [index];
+    },
+
+    [Mutations.CLEAR_SELECTS](state) {
+      state.selectIndexs = [];
     },
 
     [Mutations.UPDATE_ELEMENT_PROPS](state, { path, value }) {
@@ -76,3 +96,20 @@ export const store = createStore<State>({
     },
   },
 });
+
+function enhanceBlock(name: Component, index: number): Child {
+  return {
+    name: Block,
+    __uuid: 0,
+    children: [
+      {
+        name,
+        __uuid: new Date().getTime(),
+      },
+    ],
+    props: {
+      name: name.name,
+      index,
+    },
+  };
+}
