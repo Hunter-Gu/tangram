84a7a614e4d6a0b92a3c9bae6dfda4629d317ddb
Nail <nail.gu.hunter@gmail.com>
2021-08-05 00:24:45 +0800

    feat(plugins/element-plus.ts, pages/editor/editor/registry.ts): add temporal data for phased dev

--- src/plugins/element-plus.ts
+++ src/plugins/element-plus.ts
@@ -1,10 +1,17 @@
-import { Component } from "../core/parser/src/types/schema";
 import { App } from "@vue/runtime-core";
 import ElementPlus from "element-plus";
 import "element-plus/lib/theme-chalk/index.css";
 
+let components: any;
+
 export default {
   install(app: App) {
     app.use(ElementPlus);
+
+    components = app._context.components;
   },
 };
+
+export function getAllElementPlusComponents() {
+  return components;
+}
