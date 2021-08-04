51c01f76dd4f710879aaee16cd7cd1a195807261
Nail <nail.gu.hunter@gmail.com>
2021-08-05 00:05:51 +0800

    feat(pages/editor/props-render): hide props editor if no component instance selected

--- src/pages/editor/props-render/props-render.vue
+++ src/pages/editor/props-render/props-render.vue
@@ -1,9 +1,9 @@
 <template>
-  <SchemaRender :schema="schema"></SchemaRender>
+  <SchemaRender v-if="schema" :schema="schema"></SchemaRender>
 </template>
 
 <script lang="ts" setup>
-import { defineProps } from "@vue/runtime-core";
+import { computed, defineProps } from "@vue/runtime-core";
 import type { PropType } from "@vue/runtime-core";
 import { SchemaRender } from "../../../components";
 import { descritpor2Schema } from "../transformer";
@@ -11,10 +11,12 @@ import type { PropsDescriptor } from "../types/descriptor";
 
 const props = defineProps({
   descriptor: {
-    type: [Object, null] as PropType<PropsDescriptor | null>,
+    type: [Object] as PropType<PropsDescriptor | null | undefined>,
     required: true,
   },
 });
 
-const schema = props.descriptor || descritpor2Schema(props.descriptor!);
+const schema = computed(
+  () => props.descriptor && descritpor2Schema(props.descriptor)
+);
 </script>
