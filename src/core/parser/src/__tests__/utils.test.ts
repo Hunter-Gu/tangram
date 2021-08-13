60d9edd2bbb4537d570f66d2bbf8e0886efd97a4
Nail <nail.gu.hunter@gmail.com>
2021-08-14 01:18:24 +0800

    test(parser): add unit tests for utils of parser

--- src/core/parser/src/__tests__/utils.test.ts
+++ src/core/parser/src/__tests__/utils.test.ts
@@ -1,19 +1,73 @@
-import { isUndefined, set } from "../utils/utils";
+import { get, isUndefined, set } from "../utils/utils";
 
-describe("schema-render utils", () => {
-  test("isUndefined", () => {
+describe("Parser utils functions", () => {
+  it("It will return true when `isUndefined()` received undefined value", () => {
     const undef = undefined;
 
     expect(isUndefined(undef)).toBe(true);
   });
 
-  test("set", () => {
-    const obj = {};
-    set(obj, "key1.key2", "Hello World");
-    expect(obj).toEqual({
-      key1: {
-        key2: "Hello World",
-      },
+  describe("The `get()` will return value of target property by string path", () => {
+    it("It will return value of target property when property exists", () => {
+      const data = {
+        a: {
+          b: {
+            c: "c",
+          },
+        },
+      };
+
+      expect(get(data, "a.b.c")).toBe("c");
+    });
+
+    it("It will return defalut value when property not exists", () => {
+      const data = {
+        a: {
+          b: {
+            c: "c",
+          },
+        },
+      };
+
+      expect(get(data, "1.2.3", "a")).toBe("a");
+    });
+  });
+
+  describe("The `set()` can set the value of target property by string path", () => {
+    it("It will overwrite the value of target property when property exists", () => {
+      const obj = {
+        a: {
+          b: {
+            c: "c",
+          },
+        },
+      };
+
+      set(obj, "a.b.c", "Hello World");
+      expect(obj).toEqual({
+        a: {
+          b: {
+            c: "Hello World",
+          },
+        },
+      });
+    });
+
+    it("It will add property and set value when property not exists", () => {
+      const obj = {
+        a: "a",
+      };
+
+      set(obj, "1.2.3", "a");
+
+      expect(obj).toEqual({
+        a: "a",
+        1: {
+          2: {
+            3: "a",
+          },
+        },
+      });
     });
   });
 });
