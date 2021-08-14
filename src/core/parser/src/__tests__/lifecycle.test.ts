3162e16ec2e01ee32028983ef8d60e6185cc0087
Nail <nail.gu.hunter@gmail.com>
2021-08-14 23:04:46 +0800

    test(parser): add unit test for lifecycle() of parser

---
+++ src/core/parser/src/__tests__/lifecycle.test.ts
@@ -0,0 +1,79 @@
+import { lifecycle } from "../lifecycle";
+import { ParsedSchema } from "../types/schema";
+
+describe("Parser lifecycle", () => {
+  it("It is a dfs but The root instance will be created at last", () => {
+    const schema: ParsedSchema = {
+      __uuid: 0,
+      name: "10",
+      children: [
+        {
+          __uuid: 0,
+          name: "3",
+          children: [
+            {
+              __uuid: 0,
+              name: "2",
+              children: [
+                {
+                  __uuid: 0,
+                  name: "1",
+                },
+              ],
+            },
+          ],
+        },
+        {
+          __uuid: 0,
+          name: "6",
+          children: [
+            {
+              __uuid: 0,
+              name: "5",
+              children: [
+                {
+                  __uuid: 0,
+                  name: "4",
+                },
+              ],
+            },
+          ],
+        },
+        {
+          __uuid: 0,
+          name: "9",
+          children: [
+            {
+              __uuid: 0,
+              name: "8",
+              children: [
+                {
+                  __uuid: 0,
+                  name: "7",
+                },
+              ],
+            },
+          ],
+        },
+      ],
+    };
+    const idOrder: string[] = [];
+
+    lifecycle(schema, (node) => {
+      idOrder.push(node.name);
+    });
+
+    expect(idOrder).toEqual([
+      "1",
+      "2",
+      "3",
+      "4",
+      "5",
+      "6",
+      "7",
+      "8",
+      "9",
+      "10",
+    ]);
+  });
+});
