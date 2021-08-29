545214761dba44a509fcd95f2c094444c7d6b7cb
Nail <nail.gu.hunter@gmail.com>
2021-08-29 21:36:58 +0800

    test(pages/editor/block): add unit test for Select Wrapper of component Block

---
+++ src/pages/editor/block/hoc/__tests__/drop.test.ts
@@ -0,0 +1,48 @@
+import { mount } from "@vue/test-utils";
+import Drop from "../drop.vue";
+
+describe("HOC Drop for Block", () => {
+  it("It will clear drag focus status and emit add event when drop element", async () => {
+    const wrapper = mount(Drop, {
+      props: {
+        path: "path",
+      },
+    });
+    const dropTarget = wrapper.find("div");
+
+    await dropTarget.trigger("drop");
+
+    expect(dropTarget.classes()).toEqual([]);
+    expect(wrapper.emitted().add.length).toBe(1);
+  });
+
+  it("It will calc drag bound when dragover", async () => {
+    const wrapper = mount(Drop, {
+      props: {
+        path: "path",
+      },
+    });
+    const dropTarget = wrapper.find("div");
+
+    await dropTarget.trigger("dragover");
+
+    expect(dropTarget.classes().length).toBeTruthy();
+  });
+
+  it("It will clear drag focus status when dragleave", async () => {
+    const wrapper = mount(Drop, {
+      props: {
+        path: "path",
+      },
+    });
+    const dropTarget = wrapper.find("div");
+
+    await dropTarget.trigger("dragover");
+
+    expect(dropTarget.classes().length).toBeTruthy();
+
+    await dropTarget.trigger("dragleave");
+
+    expect(dropTarget.classes().length).toBeFalsy();
+  });
+});
