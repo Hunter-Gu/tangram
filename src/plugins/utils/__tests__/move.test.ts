87f51a33155c1b25ed0a270d4c6996795b371f66
Nail <nail.gu.hunter@gmail.com>
2021-08-30 21:47:40 +0800

    test(plugins/store): add test for move of plugin store

---
+++ src/plugins/utils/__tests__/move.test.ts
@@ -0,0 +1,59 @@
+import { getParentPathAndIndex, move } from "../move";
+
+describe("move() and getParentPathAndIndex()", () => {
+  it("should change position by move()", () => {
+    const schema = [
+      {
+        name: "a",
+      },
+      {
+        name: "b",
+      },
+      {
+        name: "c",
+      },
+      {
+        name: "d",
+      },
+      {
+        name: "e",
+      },
+    ];
+
+    move(
+      {
+        array: schema,
+        index: 1,
+      },
+      {
+        array: schema,
+        index: 3,
+      }
+    );
+
+    expect(schema).toEqual([
+      {
+        name: "a",
+      },
+      {
+        name: "c",
+      },
+      {
+        name: "d",
+      },
+      {
+        name: "b",
+      },
+      {
+        name: "e",
+      },
+    ]);
+  });
+
+  it("it will return parent path by getParentPathAndIndex()", () => {
+    expect(getParentPathAndIndex("children.0.children.1.children.2")).toEqual({
+      parentPath: "children.0.children.1.children",
+      index: "2",
+    });
+  });
+});
