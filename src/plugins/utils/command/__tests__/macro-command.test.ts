3f9fa2eccc23ababf865829c3a9cc65ca601f842
Nail <nail.gu.hunter@gmail.com>
2021-09-15 20:10:11 +0800

    feat(plugins/store): [do & undo] add MacroCommand to make serveral commands as a common command

---
+++ src/plugins/utils/command/__tests__/macro-command.test.ts
@@ -0,0 +1,95 @@
+import { AddCommand } from "../add-command";
+import { BaseCommand } from "../base-command";
+import { MacroCommand } from "../macro-command";
+import { AddCommandStatData } from "../types";
+
+function getCommandList(macro: MacroCommand) {
+  // @ts-ignore
+  return macro.commandList;
+}
+
+describe("MacroCommand", () => {
+  it("add command commandList by add()", () => {
+    const macro = new MacroCommand();
+
+    expect(getCommandList(macro).length).toBe(0);
+
+    macro.add(new AddCommand({} as AddCommandStatData));
+    macro.add(new AddCommand({} as AddCommandStatData));
+    macro.add(new AddCommand({} as AddCommandStatData));
+
+    expect(getCommandList(macro).length).toBe(3);
+  });
+
+  it("get command at position index in commandList by get()", () => {
+    const macro = new MacroCommand();
+
+    expect(getCommandList(macro).length).toBe(0);
+
+    macro.add(new AddCommand({ path: "path1" } as AddCommandStatData));
+    macro.add(new AddCommand({ path: "path2" } as AddCommandStatData));
+    macro.add(new AddCommand({ path: "path3" } as AddCommandStatData));
+
+    expect(macro.get(1)).toEqual(
+      new AddCommand({ path: "path2" } as AddCommandStatData)
+    );
+  });
+
+  it("should execute all commands by do()", () => {
+    const command1 = {
+      do: jest.fn().mockReturnValue({ schema: 1 }),
+    } as unknown as BaseCommand;
+    const command2 = {
+      do: jest.fn().mockReturnValue({ schema: 2 }),
+    } as unknown as BaseCommand;
+    const command3 = {
+      do: jest.fn().mockReturnValue({ schema: 3 }),
+    } as unknown as BaseCommand;
+    const macro = new MacroCommand();
+
+    macro.add(command1);
+    macro.add(command2);
+    macro.add(command3);
+
+    // @ts-ignore
+    const result = macro.do(0);
+
+    expect(command1.do).toBeCalledWith(0);
+    expect(command2.do).toBeCalledWith(1);
+    expect(command3.do).toBeCalledWith(2);
+    expect(result).toEqual(
+      expect.objectContaining({
+        schema: 3,
+      })
+    );
+  });
+
+  it("should execute all commands in reserved order by undo()", () => {
+    const command1 = {
+      undo: jest.fn().mockReturnValue({ schema: 1 }),
+    } as unknown as BaseCommand;
+    const command2 = {
+      undo: jest.fn().mockReturnValue({ schema: 2 }),
+    } as unknown as BaseCommand;
+    const command3 = {
+      undo: jest.fn().mockReturnValue({ schema: 3 }),
+    } as unknown as BaseCommand;
+    const macro = new MacroCommand();
+
+    macro.add(command1);
+    macro.add(command2);
+    macro.add(command3);
+
+    // @ts-ignore
+    const result = macro.undo(0);
+
+    expect(command3.undo).toBeCalledWith(0);
+    expect(command2.undo).toBeCalledWith(3);
+    expect(command1.undo).toBeCalledWith(2);
+    expect(result).toEqual(
+      expect.objectContaining({
+        schema: 1,
+      })
+    );
+  });
+});
---
+++ src/plugins/utils/command/macro-command.ts
@@ -0,0 +1,36 @@
+import { SchemaData } from "../../../core/parser/src/types/schema";
+import { BaseCommand } from "./base-command";
+import { Command, OperationResult } from "./types";
+
+export class MacroCommand implements Command<SchemaData, OperationResult> {
+  private commandList: BaseCommand<unknown>[] = [];
+
+  get(index: number) {
+    return this.commandList[index] as BaseCommand<unknown> | undefined;
+  }
+
+  add(command: BaseCommand) {
+    this.commandList.push(command);
+  }
+
+  do(schemaData: SchemaData) {
+    return this.commandList.reduce(
+      ({ schema }, command) => {
+        return command.do(schema);
+      },
+      { schema: schemaData }
+    ) as OperationResult;
+  }
+
+  undo(schemaData: SchemaData) {
+    return this.commandList
+      .slice()
+      .reverse()
+      .reduce(
+        ({ schema }, command) => {
+          return command.undo(schema);
+        },
+        { schema: schemaData }
+      ) as OperationResult;
+  }
+}
